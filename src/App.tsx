42525ca2b552349cdbb35861f8dcd489bd59301e
Ruiran Oliveira <ruiranoliveirasantos@gmail.com>
2020-07-29 21:39:37 -0300

    adding the table component to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Header from './header/index';
+import Table from './table/index';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Table />
     </div>
   );
 }
